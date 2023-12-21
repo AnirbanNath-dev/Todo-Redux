5ab973047b42c3b8c9e73a00f1e3ede3318de56f
Anirban <anirbaniitian@gmail.com>
2023-12-21 14:33:08 +0530

    Removed framer motion

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,7 +5,7 @@ import { TiTickOutline } from "react-icons/ti";
 import { TiTick } from "react-icons/ti";
 import { useDispatch, useSelector } from 'react-redux';
 import { completedTodo , removeTodo } from '../redux/features/slices';
-import { motion , AnimatePresence} from 'framer-motion'
+
 
 function Tasks({completed , todo , id}) {
   // const isCompleted = useSelector(state => state.todos.filter(todo => todo.id == id))
@@ -33,20 +33,7 @@ function Tasks({completed , todo , id}) {
 
 
   return (
-    <>
-    <AnimatePresence>
-    <motion.div 
-    className={`w-[35vw] border-[1px]  hover:scale-110 transition-all duration-500 cursor-pointer rounded-xl flex items-center text-lg tracking-wide justify-between px-5 py-2 gap-2 ${completed ? 'bg-green-500 line-through' : 'bg-transparent'} ${isReadOnly ? 'border-black' : 'border-red-500 shadow-md shadow-red-300' }`}
-    
-    animate = {{x: 0 , opacity: 1}}
-    initial = {{x : -100  , opacity: 0}}
-    transition={{duration : 0.1}}
-
-    
-    
-    >
-      
-      
+    <div className={`w-[35vw] border-[1px]  hover:scale-110 transition-all duration-500 cursor-pointer rounded-xl flex items-center text-lg tracking-wide justify-between px-5 py-2 gap-2 ${completed ? 'bg-green-500 line-through' : 'bg-transparent'} ${isReadOnly ? 'border-black' : 'border-red-500 shadow-md shadow-red-300' }`}>
       {
         completed ? <TiTick onClick={doneUndoneFn}/> : <TiTickOutline onClick={doneUndoneFn}/>
       }
@@ -62,9 +49,7 @@ function Tasks({completed , todo , id}) {
         <FaEdit onClick={editFn} className={`${isReadOnly ? '': 'text-yellow-600'}`}/>
         <MdDelete className='text-red-600' onClick={deleteFn}/>
       </div>
-    </motion.div>
-    </AnimatePresence>
-    </>
+    </div>
   )
 }
 
